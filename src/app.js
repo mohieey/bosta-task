27ba28f48af56513cb95c3bbb9ee251fe5f01ba6
mohieey <mohhiey@gmail.com>
2021-11-24 02:39:35 +0200

    Restructured the app to prepare for testing

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+//Routes
+const users = require("./routes/users");
+const verify = require("./routes/verify");
+const checks = require("./routes/checks");
+const reports = require("./routes/reports");
+
+const app = express();
+app.use(express.json());
+
+app.use("/api/", users);
+app.use("/api/verify", verify);
+app.use("/api/check", checks);
+app.use("/api/report", reports);
+
+module.exports = app;
--- src/index.js
+++ src/index.js
@@ -1,23 +1,6 @@
-const express = require("express");
 const mongoose = require("mongoose");
+const app = require("./app");
 const { PORT, MongoConnectionString } = require("./env");
-//Routes
-const users = require("./routes/users");
-const verify = require("./routes/verify");
-const checks = require("./routes/checks");
-const reports = require("./routes/reports");
-
-const app = express();
-app.use(express.json());
-
-app.use("/api/", users);
-app.use("/api/verify", verify);
-app.use("/api/check", checks);
-app.use("/api/report", reports);
-
-app.get("/", (req, res) => {
-  throw new Error("");
-});
 
 app.listen(PORT, () => {
   console.log(`Listening on http://localhost:${PORT}`);
