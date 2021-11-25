4aa300e6350316252e400352d7c1e7f65120a392
mohieey <mohhiey@gmail.com>
2021-11-25 13:48:22 +0200

    modified the port to listen on

--- src/env.js
+++ src/env.js
@@ -1,4 +1,4 @@
-module.exports.PORT = process.env.PORT || 3001;
+module.exports.PORT = process.env.PORT || 3002;
 module.exports.MongoConnectionString =
   process.env.MongoConnectionString || "mongodb://localhost:27017/monitorbot";
 module.exports.SALT = process.env.SALT || "euriostghoiuerhftg";
